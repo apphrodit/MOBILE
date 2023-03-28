1b3e55182f05d4f39ec983fa42cced4b1822f37d
apphrodit <joaovrdf@gmail.com>
2023-03-28 11:31:27 -0300

    MOBILE

---
+++ Exercicio/App.js
@@ -0,0 +1,20 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F36863',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
