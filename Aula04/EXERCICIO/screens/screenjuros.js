fb7ba92c532a5d6ae6ca28b39bc23f1cf99a69ad
apphrodit <joaovrdf@gmail.com>
2023-05-02 11:22:41 -0300

    MOBILE

---
+++ Aula04/EXERCICIO/screens/screenjuros.js
@@ -0,0 +1,11 @@
+import FormJuros from "../components/loginform";
+import { View } from "react-native";
+
+export default function ScreenJuros() {
+    
+    return(
+        <View>
+            <Text>Componente Juros</Text>
+        </View>
+    )
+}
---
+++ aula05/clientes/source/screens/clientesScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View,Text, FlatList, StyleSheet } from 'react-native'
+import clientes from '../mocks/clientesMock'
+
+export default function ClientesScreen ({ navigation }){
+    return(
+        <View style={styles.container}>
+            <FlatList
+            data={clientes}
+            renderItem ={({item}) =>{
+                return<>
+                <Text style={styles.text}>{item.cpf}</Text>
+                <Text style={styles.text}>{item.nome} {item.sobrenome}</Text>
+                <Text style={styles.text}>{item.telefone}</Text>
+            </>}}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex :1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    list: {
+        width: '100%',
+        paddingHorizontal: 20
+    },
+    text: {
+        fontSize: 20
+    }
+});
